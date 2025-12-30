bfebe6b5eaba441c3eb6d19d344bd611ddc4fa15
YashMandlekar <mandlekarvansh@gmail.com>
2025-12-30 13:51:59 +0530

    Update Practical1.cpp

--- Practical1.cpp
+++ Practical1.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+// #1 sum program
+#include <iostream.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    cout << sum;
     return 0;
+}
+
+
+
+// for average case
+#include <iostream.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter any number";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        cout << i;
+    }
+    return 0;
+}
+
+
+// for best case
+#include <iostream.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter any no:";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            cout << i << j;
+        }
+    }
 }
